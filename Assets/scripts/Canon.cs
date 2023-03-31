9e4a5a7b7caa15d18974c59cf8b318a018cb6095
NikeshDC <nikeshdc111@gmail.com>
2023-03-31 10:50:49 +0545

    initialized projects
    contains 3D scene with a floating island, house, cannon some physics for cannon and 3D audio

---
+++ Assets/scripts/Canon.cs
@@ -0,0 +1,102 @@
+﻿
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Canon : MonoBehaviour {
+
+    public Transform firepoint;
+    public GameObject canonballprefab;
+    public GameObject smokecloud;
+    GameObject canonball;
+    public float cannonForce = 20.0f;
+    public float rotationSpeed = 60.0f;
+    bool canRotate = true;
+    bool canFire = true;
+    public Transform barrel;
+    public Transform canonbase;
+    public AudioSource firingAudio;
+    //public Canvas UI;
+
+    private void Update()
+    {
+        if (canRotate)
+        {
+            getInput();
+        }
+
+        if(canFire)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+                {
+                fire();
+                }
+        }
+    }
+
+
+    void getInput()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotateLeft();
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotateRight();      
+        }
+
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotateUp();
+        }
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotateDown();
+        }
+    }
+
+
+    public void rotateUp()
+    {
+        rotateBarrel(-1);
+    }
+
+   public void rotateDown()
+    {
+        rotateBarrel(1);
+    }
+
+    public void rotateRight()
+    {
+        rotateCanon(1);
+    }
+
+    public void rotateLeft()
+    {
+        rotateCanon(-1);
+    }
+
+
+    void rotateCanon(int dir)
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime *dir);
+    }
+
+    void rotateBarrel(int dir)
+    {
+        barrel.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * dir,Space.Self);
+    }
+
+    public void fire()
+    {
+        firingAudio.Play();
+        canonball =  Instantiate(canonballprefab, firepoint.transform.position, firepoint.transform.rotation);
+        canonball.GetComponent<Rigidbody>().AddForce(firepoint.forward * cannonForce);
+        Destroy(canonball, 5.0f);
+
+        GameObject smokeClouds = Instantiate(smokecloud, firepoint.transform.position, Quaternion.identity);
+        Destroy(smokeClouds, 5.0f);
+    }
+}
---
+++ Assets/scripts/Canonball.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class Canonball : MonoBehaviour
+{
+    public GameObject explosion;
+
+    public float explosionForce = 30f;
+    public float explosionRange =  5f;
+
+    public AudioSource impactAudio;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        impactAudio.Play();
+        if (collision.collider.tag == "target")
+        {
+           //on_impact_explode();
+        }
+    }
+
+    public void on_impact_explode()
+    {
+        GameObject explosionObj = Instantiate(explosion, transform.position, Quaternion.identity);
+        Collider[] collidingObjects = Physics.OverlapSphere(transform.position, explosionRange);
+        Rigidbody collidingObjectsRb;
+        foreach (Collider collidingObject in collidingObjects)
+        {
+            collidingObjectsRb = collidingObject.gameObject.GetComponent<Rigidbody>();
+            if (collidingObjectsRb != null)
+                collidingObjectsRb.AddExplosionForce(explosionForce, transform.position, explosionRange);
+        }
+        Destroy(explosionObj, 5f);
+        Destroy(this.gameObject);
+    }
+}
